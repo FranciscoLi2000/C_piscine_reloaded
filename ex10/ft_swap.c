15f52e2f337877275ffb65a22d38606d4ddaed18
FranciscoLi2000 <francisco2000li@icloud.com>
2024-12-18 15:26:22 +0000

    42Barcelona

--- ex10/ft_swap.c
+++ ex10/ft_swap.c
@@ -22,7 +22,10 @@ void	ft_swap(int *a, int *b)
 	*b = temp;
 }
 
-int	main(int argc, char **argv)
+int	main(int argc, char **argv)	
+// Main Function Parameter Types:
+// int argc: Represents the number of command-line arguments; Always an integer; Includes the program name as the first argument.
+// char **argv: An array of strings (character pointers); Each element is a pointer to a command-line argument string.
 {
 	int x = 5;
 	int y = 6;
