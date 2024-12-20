368665b26e9f750918af6e8ca74b2fa3989be5ae
FranciscoLi2000 <francisco2000li@icloud.com>
2024-12-20 23:30:22 +0000

    42Barcelona

---
+++ ex12/ft_iterative_factorial.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yufli <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/21 00:13:21 by yufli             #+#    #+#             */
+/*   Updated: 2024/12/21 00:25:43 by yufli            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+int	ft_iterative_factorial(int nb)
+{
+	int	i;
+	int	result;
+
+	if (nb < 0)
+		return (0);
+	else
+	{
+		i = 1;
+		result = 1;
+		while (i <= nb)
+		{
+			result = result * i;
+			i++;
+		}
+		return (result);
+	}
+}
+
+int	main(void)
+{
+	int	num;
+
+	num = 7;
+	printf("%i\n", ft_iterative_factorial(num));
+	return (0);
+}
